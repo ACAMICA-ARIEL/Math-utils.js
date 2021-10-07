85541c36c741e2f8061915c02baa916bcb58ee14
ArielPenna <arielpenna@gmail.com>
2021-10-07 08:58:56 -0300

    Refactor: Replace console.logs with return

--- MathUtils.js
+++ MathUtils.js
@@ -1,33 +1,33 @@
 function add(num1, num2) {
-  console.log('The addition is: ', num1 + num2);
+	return num1 + num2
 }
 
 function subtract(num1, num2) {
-  console.log('The subtraction is: ', num1 - num2);
+	return num1 - num2
 }
 
 function multiply(num1, num2) {
-  console.log('The multiplication is: ', num1 * num2);
+	return num1 * num2
 }
 
 function divide(num1, num2) {
-  console.log('The division is: ', num1 / num2);
+	return num1 / num2
 }
 
 function esMenor(num1, num2) {
-  if (num1 < num2) return num1;
-  return num2;
+	if (num1 < num2) return num1
+	return num2
 }
 
 function potencia(num1, num2) {
-  let potencia = Math.pow(num1, num2);
-  return potencia;
+	let potencia = Math.pow(num1, num2)
+	return potencia
 }
 
 function isCynthia(name) {
-  if (name === 'Cynthia') {
-    return true;
-  } else {
-    return false;
-  }
+	if (name === 'Cynthia') {
+		return true
+	} else {
+		return false
+	}
 }
